287548112162828282782fe1680e547c9de67697
thief-sty <11363816+thief-sty@users.noreply.github.com>
2026-09-24 18:49:51 +0200

    feat: parse pages
    LLM assistance (Claude Sonnet 5): for fetching usage recommendation (from `ftml` docs) for `ftml::parse`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::path::PathBuf;
+use std::{borrow::Cow, path::PathBuf};
 
 use clap::Parser;
+use ftml::{
+    data::{PageInfo, ScoreValue},
+    layout::Layout,
+    prelude::{WikitextMode, WikitextSettings},
+};
 use rusqlite::Connection;
 
 #[derive(Parser)]
@@ -9,6 +14,13 @@ struct Cli {
     db: PathBuf,
     /// The Wikidot site handle (e.g. `scp-wiki`)
     site: String,
+}
+
+struct Page {
+    slug: String,
+    source: String,
+    title: String,
+    category: String,
 }
 
 fn main() {
@@ -21,8 +33,38 @@ fn main() {
         .expect("Failed to prepare SQL `SELECT` statement");
     let pattern = format!("http://{}.wikidot.com/%", args.site);
     let page_iter = stmt
-        .query_map([pattern], |row| Ok(row.get::<_, String>("slug")?))
+        .query_map([pattern], |row| {
+            Ok(Page {
+                slug: row.get("slug")?,
+                source: row.get("source")?,
+                title: row.get("title")?,
+                category: row.get("category")?,
+            })
+        })
         .expect("Failed to query the database");
 
-    dbg!(page_iter.collect::<Vec<_>>().len());
+    let parse_settings = WikitextSettings::from_mode(WikitextMode::Page, Layout::Wikidot);
+
+    for mut page in
+        page_iter.map(|maybe_page| maybe_page.expect("Failed to iterate through a page"))
+    {
+        let page_info = PageInfo {
+            page: Cow::Borrowed(&page.slug),
+            category: match &*page.category {
+                "_default" => None, // according to docs: https://docs.rs/ftml/1.41.0/ftml/data/struct.PageInfo.html#structfield.category
+                category => Some(Cow::Borrowed(category)),
+            },
+            site: Cow::Borrowed(&args.site),
+            title: Cow::Borrowed(&page.title),
+            alt_title: None,               // not worth the hassle to try to fetch it
+            score: ScoreValue::Integer(0), // same
+            tags: Vec::new(),              // same
+            language: Cow::Borrowed("en"), // same
+        };
+
+        ftml::preprocess(&mut page.source);
+        let tokens = ftml::tokenize(&page.source);
+        let (tree, warnings) = ftml::parse(&tokens, &page_info, &parse_settings).into();
+        println!("page `{}`: {} warnings", page.slug, warnings.len());
+    }
 }
