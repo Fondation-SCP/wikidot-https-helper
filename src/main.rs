555461be961ec38bd063a9481920481a9c7295d2
thief-sty <11363816+thief-sty@users.noreply.github.com>
2026-09-24 17:09:13 +0200

    feat: connect to a sqlite db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use rusqlite::Connection;
+
+#[derive(Parser)]
+struct Cli {
+    db: PathBuf,
+    site: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let db = Connection::open(args.db).expect("Failed to open a connection to the database");
+
+    let mut stmt = db
+        .prepare("SELECT * FROM pages WHERE url LIKE ?")
+        .expect("Failed to prepare SQL `SELECT` statement");
+    let pattern = format!("http://{}.wikidot.com/%", args.site);
+    let page_iter = stmt
+        .query_map([pattern], |row| Ok(row.get::<_, String>("slug")?))
+        .expect("Failed to query the database");
+
+    dbg!(page_iter.collect::<Vec<_>>().len());
 }
